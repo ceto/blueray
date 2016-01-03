827724022c26b04e573b8dbd2dd5738dc34b3dd1
Gabor Szabo <szabogabi@gmail.com>
2016-01-03 22:23:54 +0100

    ds stroe deleted

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,25 +1,19 @@
+'use strict';
 $(document).foundation();
 $(document).ready( function() {
-
-  new WOW({
-     callback:     function(box) {
-      //alert(box);
-    },
-  }).init();
-
-  var circle = new Array;
+  new WOW().init();
+  var circle = [];
 
   $('.fact__ill').each( function(index) {
     $(this).css('color', $(this).attr('data-color'));
-    circle['index'] = new ProgressBar.Circle('#f'+index, {
-      color: $('#f'+index).attr('data-color'),
+    circle['index'] = new ProgressBar.Circle('#f' + index, {
+      color: $('#f' + index).attr('data-color'),
       strokeWidth: 3,
-      trailColor:'#fff',
+      trailColor: '#fff',
       fill: '#fff',
       easing: 'easeInOut',
       duration: 1500
     });
-    circle['index'].animate($('#f'+index).attr('data-meter'));
+    circle['index'].animate($('#f' + index).attr('data-meter'));
   });
-
 });
