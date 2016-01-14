469a364bc90347abba934e4e902d87a0759b1183
Gabor Szabo <szabogabi@gmail.com>
2016-01-14 15:00:00 +0100

    Update 2016-01-14T13:59:59.626Z

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,20 @@
-"use strict";$(document).foundation(),$(document).ready(function(){(new WOW).init();var t=[];$(".fact__ill").each(function(a){$(this).css("color",$(this).attr("data-color")),t[a]=new ProgressBar.Circle("#f"+a,{color:$("#f"+a).attr("data-color"),strokeWidth:3,trailColor:"#fff",fill:"#fff",easing:"easeInOut",duration:1500}),t[a].animate($("#f"+a).attr("data-meter"))})});
+'use strict';
+
+$(document).foundation();
+$(document).ready( function() {
+  new WOW().init();
+  var circle = [];
+
+  $('.fact__ill').each( function(index) {
+    $(this).css('color', $(this).attr('data-color'));
+    circle[index] = new ProgressBar.Circle('#f' + index, {
+      color: $('#f' + index).attr('data-color'),
+      strokeWidth: 3,
+      trailColor: '#fff',
+      fill: '#fff',
+      easing: 'easeInOut',
+      duration: 1500
+    });
+    circle[index].animate($('#f' + index).attr('data-meter'));
+  });
+});
